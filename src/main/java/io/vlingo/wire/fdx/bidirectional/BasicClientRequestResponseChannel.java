747b441ceda6b36258aa70f8efb348f4d511e50d
VaughnVernon <vaughn@forcomprehension.com>
2019-07-15 22:48:14 -0600

    Reimplemented.

--- src/main/java/io/vlingo/wire/fdx/bidirectional/BasicClientRequestResponseChannel.java
+++ src/main/java/io/vlingo/wire/fdx/bidirectional/BasicClientRequestResponseChannel.java
@@ -7,14 +7,24 @@
 
 package io.vlingo.wire.fdx.bidirectional;
 
-import io.vlingo.actors.Logger;
-import io.vlingo.wire.channel.ResponseChannelConsumer;
-import io.vlingo.wire.node.Address;
-
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
-public class BasicClientRequestResponseChannel extends ClientRequestResponseChannel {
+import io.vlingo.actors.Logger;
+import io.vlingo.wire.channel.ResponseChannelConsumer;
+import io.vlingo.wire.message.ByteBufferPool;
+import io.vlingo.wire.message.ConsumerByteBuffer;
+import io.vlingo.wire.node.Address;
+
+public class BasicClientRequestResponseChannel implements ClientRequestResponseChannel {
+  private final Address address;
+  private final ResponseChannelConsumer consumer;
+  private final Logger logger;
+  private final ByteBufferPool readBufferPool;
+
+  private SocketChannel channel;
   private int previousPrepareFailures;
 
   public BasicClientRequestResponseChannel(
@@ -24,43 +34,114 @@ public class BasicClientRequestResponseChannel extends ClientRequestResponseChan
           final int maxMessageSize,
           final Logger logger)
   throws Exception {
-    super(address, consumer, maxBufferPoolSize, maxMessageSize, logger);
-
+    this.address = address;
+    this.consumer = consumer;
+    this.logger = logger;
+    this.readBufferPool = new ByteBufferPool(maxBufferPoolSize, maxMessageSize);
     this.previousPrepareFailures = 0;
   }
 
-  /**
-   * @see io.vlingo.wire.fdx.bidirectional.ClientRequestResponseChannel#preparedChannelDelegate()
-   */
-  @Override
-  protected SocketChannel preparedChannelDelegate() {
-    SocketChannel channel = channel();
+  //=========================================
+  // RequestSenderChannel
+  //=========================================
 
+  @Override
+  public void close() {
+    if (channel != null) {
+      try {
+        channel.close();
+      } catch (Exception e) {
+        logger.error("Failed to close channel to " + address + " because: " + e.getMessage(), e);
+      }
+    }
+    channel = null;
+  }
+
+  @Override
+  public void requestWith(final ByteBuffer buffer) {
+    final SocketChannel preparedChannel = preparedChannel();
+
+    if (preparedChannel != null) {
+      try {
+        while (buffer.hasRemaining()) {
+          preparedChannel.write(buffer);
+        }
+      } catch (Exception e) {
+        logger.error("Write to socket failed because: " + e.getMessage(), e);
+        close();
+      }
+    }
+  }
+
+
+  //=========================================
+  // ResponseListenerChannel
+  //=========================================
+
+  @Override
+  public void probeChannel() {
+    try {
+      final SocketChannel channel = preparedChannel();
+      if (channel != null) {
+        readConsume(channel);
+      }
+    } catch (IOException e) {
+      logger.error("Failed to read channel selector for " + address + " because: " + e.getMessage(), e);
+    }
+  }
+
+  //=========================================
+  // internal implementation
+  //=========================================
+
+  private SocketChannel preparedChannel() {
     try {
       if (channel != null) {
         if (channel.isConnected()) {
           previousPrepareFailures = 0;
           return channel;
         } else {
-          closeChannel();
+          close();
         }
       } else {
         channel = SocketChannel.open();
-        channel.connect(new InetSocketAddress(address().hostName(), address().port()));
+        channel.connect(new InetSocketAddress(address.hostName(), address.port()));
         channel.configureBlocking(false);
         previousPrepareFailures = 0;
         return channel;
       }
     } catch (Exception e) {
-      closeChannel();
+      close();
       final String message = getClass().getSimpleName() + ": Cannot prepare/open channel because: " + e.getMessage();
       if (previousPrepareFailures == 0) {
-        logger().error(message, e);
+        logger.error(message, e);
       } else if (previousPrepareFailures % 20 == 0) {
-        logger().info("AGAIN: " + message);
+        logger.info("AGAIN: " + message);
       }
     }
     ++previousPrepareFailures;
     return null;
+  }
+
+  private void readConsume(final SocketChannel channel) throws IOException {
+    final ConsumerByteBuffer pooledBuffer = readBufferPool.accessFor("client-response", 25);
+    final ByteBuffer readBuffer = pooledBuffer.asByteBuffer();
+    int totalBytesRead = 0;
+    int bytesRead = 0;
+    try {
+      do {
+        bytesRead = channel.read(readBuffer);
+        totalBytesRead += bytesRead;
+      } while (bytesRead > 0);
+
+      if (totalBytesRead > 0) {
+        consumer.consume(pooledBuffer.flip());
+      } else {
+        pooledBuffer.release();
+      }
+    } catch (Exception e) {
+      pooledBuffer.release();
+      throw e;
+    }
   }
 }
