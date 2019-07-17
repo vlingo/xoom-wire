ef649b059b18387fb4e1a50fea9c8545f60ecd4f
greg <greg@colorings.us>
2019-07-17 11:31:26 -0600

    remove unused TestUntil (part of issue 3)

--- src/test/java/io/vlingo/wire/fdx/bidirectional/TestRequestChannelConsumerProvider.java
+++ src/test/java/io/vlingo/wire/fdx/bidirectional/TestRequestChannelConsumerProvider.java
@@ -1,11 +1,9 @@
 package io.vlingo.wire.fdx.bidirectional;
 
-import io.vlingo.actors.testkit.TestUntil;
 import io.vlingo.wire.channel.RequestChannelConsumer;
 import io.vlingo.wire.channel.RequestChannelConsumerProvider;
 
 public class TestRequestChannelConsumerProvider implements RequestChannelConsumerProvider {
-  public TestUntil until;
   public RequestChannelConsumer consumer = new TestRequestChannelConsumer();
 
   @Override
