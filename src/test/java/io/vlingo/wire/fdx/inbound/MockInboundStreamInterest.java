56e90949559a675babdb9190f733e6ceda85af40
Eliezio Oliveira <eliezio.oliveira@est.tech>
2019-08-05 05:11:42 +0100

    Replaced TestUntil by AccessSafely on InboundStreamTest [GH-3].

--- src/test/java/io/vlingo/wire/fdx/inbound/MockInboundStreamInterest.java
+++ src/test/java/io/vlingo/wire/fdx/inbound/MockInboundStreamInterest.java
@@ -7,32 +7,58 @@
 
 package io.vlingo.wire.fdx.inbound;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 
-import io.vlingo.actors.testkit.TestUntil;
+import io.vlingo.actors.testkit.AccessSafely;
 import io.vlingo.wire.message.AbstractMessageTool;
 import io.vlingo.wire.message.RawMessage;
 import io.vlingo.wire.node.AddressType;
 
 public class MockInboundStreamInterest extends AbstractMessageTool implements InboundStreamInterest {
-  public TestResults testResults = new TestResults();
+  public TestResults testResults;
 
-  public MockInboundStreamInterest() { }
+  public MockInboundStreamInterest(int happenings) {
+    testResults = new TestResults(happenings);
+  }
   
   @Override
   public void handleInboundStreamMessage(final AddressType addressType, final RawMessage message) {
     final String textMessage = message.asTextMessage();
-    testResults.messages.add(textMessage);
-    testResults.messageCount.incrementAndGet();
-    System.out.println("INTEREST: " + textMessage + " list-size: " + testResults.messages.size() + " count: " + testResults.messageCount.get() + " count-down: " + testResults.untilStops.remaining());
-    testResults.untilStops.happened();
+    testResults.addMessage(textMessage);
+    System.out.printf("INTEREST: %s; count: %s; count-down: %s%n",
+            textMessage, testResults.messageCount(), testResults.remaining());
   }
 
   static class TestResults {
-    public final AtomicInteger messageCount = new AtomicInteger(0);
-    public final List<String> messages = new CopyOnWriteArrayList<>();
-    public TestUntil untilStops;
+    private static final String ID_MESSAGES = "messages";
+
+    private final int happenings;
+    private final AccessSafely accessSafely;
+
+    TestResults(int happenings) {
+      this.happenings = happenings;
+      List<String> messages = new ArrayList<>();
+      this.accessSafely = AccessSafely
+              .afterCompleting(happenings)
+              .writingWith(ID_MESSAGES, msg -> messages.add((String) msg))
+              .readingWith(ID_MESSAGES, () -> new ArrayList<>(messages));
+    }
+
+    void addMessage(String msg) {
+      accessSafely.writeUsing(ID_MESSAGES, msg);
+    }
+
+    List<String> getMessages() {
+      return accessSafely.readFrom(ID_MESSAGES);
+    }
+
+    int messageCount() {
+      return accessSafely.totalWrites();
+    }
+
+    int remaining() {
+      return happenings - messageCount();
+    }
   }
 }
