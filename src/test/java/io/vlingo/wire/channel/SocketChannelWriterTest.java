ffe033138a11074ee24868c005a3b1a2dcc3d031
VaughnVernon <vaughn@forcomprehension.com>
2020-03-26 23:14:09 -0700

    Refactored probeUntilConsumed.

--- src/test/java/io/vlingo/wire/channel/SocketChannelWriterTest.java
+++ src/test/java/io/vlingo/wire/channel/SocketChannelWriterTest.java
@@ -7,8 +7,15 @@
 
 package io.vlingo.wire.channel;
 
-import io.vlingo.actors.Logger;
+import static org.junit.Assert.assertEquals;
 
+import java.nio.ByteBuffer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import io.vlingo.actors.Logger;
 import io.vlingo.actors.testkit.AccessSafely;
 import io.vlingo.wire.fdx.inbound.tcp.SocketChannelInboundReader;
 import io.vlingo.wire.message.AbstractMessageTool;
@@ -18,48 +25,41 @@ import io.vlingo.wire.node.Host;
 import io.vlingo.wire.node.Id;
 import io.vlingo.wire.node.Name;
 import io.vlingo.wire.node.Node;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-
-import java.nio.ByteBuffer;
-
-import static org.junit.Assert.assertEquals;
 
 public class SocketChannelWriterTest extends AbstractMessageTool {
   private static final String TestMessage = "TEST ";
-  
+
   private SocketChannelWriter channelWriter;
   private ChannelReader channelReader;
-  
+
   @Test
   public void testChannelWriter() throws Exception {
     final MockChannelReaderConsumer consumer = new MockChannelReaderConsumer();
     final AccessSafely consumerAccess = consumer.afterCompleting(0);
-    
+
     channelReader.openFor(consumer);
-    
+
     final ByteBuffer buffer = ByteBufferAllocator.allocate(1024);
-    
+
     final String message1 = TestMessage + 1;
     final RawMessage rawMessage1 = RawMessage.from(0, 0, message1);
     channelWriter.write(rawMessage1, buffer);
-    
+
     probeUntilConsumed(channelReader, consumerAccess);
-    
+
     assertEquals(1, (int)consumerAccess.readFrom("consumeCount"));
     assertEquals(message1, consumerAccess.readFrom("message", 0));
 
     final String message2 = TestMessage + 2;
     final RawMessage rawMessage2 = RawMessage.from(0, 0, message2);
     channelWriter.write(rawMessage2, buffer);
-    
+
     probeUntilConsumed(channelReader, consumerAccess);
-    
+
     assertEquals(2, (int)consumerAccess.readFrom("consumeCount"));
     assertEquals(message2, consumerAccess.readFrom("message", 1));
   }
-  
+
   @Before
   public void setUp() throws Exception {
     final Node node = Node.with(Id.of(2), Name.of("node2"), Host.of("localhost"), 37373, 37374);
@@ -67,20 +67,24 @@ public class SocketChannelWriterTest extends AbstractMessageTool {
     channelWriter = new SocketChannelWriter(node.operationalAddress(), logger);
     channelReader = new SocketChannelInboundReader(node.operationalAddress().port(), "test-reader", 1024, logger);
   }
-  
+
   @After
   public void tearDown() {
     channelWriter.close();
     channelReader.close();
   }
 
-  private void probeUntilConsumed(final ChannelReader reader, final AccessSafely consumerAccess ) {
-    final int currentConsumedCount = consumerAccess.readFrom("consumeCount");
-    
+  private void probeUntilConsumed(final ChannelReader reader, final AccessSafely consumerAccess) {
+    final int previousConsumedCount = consumerAccess.readFrom("consumeCount");
+
     for (int idx = 0; idx < 100; ++idx) {
       reader.probeChannel();
-      
-      if ((int)consumerAccess.readFrom("consumeCount") > currentConsumedCount) break;
+
+      final int currentConsumedCount = consumerAccess.readFrom("consumeCount");
+
+      if (currentConsumedCount > previousConsumedCount) {
+        break;
+      }
     }
   }
 }
