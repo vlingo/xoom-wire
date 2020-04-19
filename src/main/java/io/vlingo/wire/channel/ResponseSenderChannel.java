87dd69fc0f0b0ec0f39ebf1a8c0e6894b4306254
VaughnVernon <vaughn@forcomprehension.com>
2020-04-19 00:58:06 -0700

    Removed explicitClose().

--- src/main/java/io/vlingo/wire/channel/ResponseSenderChannel.java
+++ src/main/java/io/vlingo/wire/channel/ResponseSenderChannel.java
@@ -11,7 +11,6 @@ import io.vlingo.wire.message.ConsumerByteBuffer;
 
 public interface ResponseSenderChannel {
   void abandon(final RequestResponseContext<?> context);
-  void explicitClose(final RequestResponseContext<?> requestResponseContext, final boolean option);
   void respondWith(final RequestResponseContext<?> context, final ConsumerByteBuffer buffer);
   void respondWith(final RequestResponseContext<?> context, final ConsumerByteBuffer buffer, final boolean closeFollowing);
 }
