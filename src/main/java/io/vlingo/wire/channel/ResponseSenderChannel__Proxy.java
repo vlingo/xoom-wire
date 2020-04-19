fc7d67a3b0376d750acec04f177cf3c3a7cd863c
VaughnVernon <vaughn@forcomprehension.com>
2020-04-19 00:58:38 -0700

    Removed explicitClose().

--- src/main/java/io/vlingo/wire/channel/ResponseSenderChannel__Proxy.java
+++ src/main/java/io/vlingo/wire/channel/ResponseSenderChannel__Proxy.java
@@ -16,8 +16,7 @@ import io.vlingo.wire.message.ConsumerByteBuffer;
 
 public class ResponseSenderChannel__Proxy implements ResponseSenderChannel {
   private static final String representationAbondon1 = "abandon(RequestResponseContext<?>)";
-  private static final String representationKeepAlive2 = "keepAlive(RequestResponseContext<?>)";
-  private static final String representationRespondWith3 = "respondWith(RequestResponseContext<?>, ConsumerByteBuffer, boolean)";
+  private static final String representationRespondWith2 = "respondWith(RequestResponseContext<?>, ConsumerByteBuffer, boolean)";
 
   private final Actor actor;
   private final Mailbox mailbox;
@@ -38,16 +37,6 @@ public class ResponseSenderChannel__Proxy implements ResponseSenderChannel {
   }
 
   @Override
-  public void explicitClose(final RequestResponseContext<?> context, final boolean option) {
-    if (!actor.isStopped()) {
-      final SerializableConsumer<ResponseSenderChannel> consumer = (actor) -> actor.explicitClose(context, option);
-      mailbox.send(new LocalMessage<ResponseSenderChannel>(actor, ResponseSenderChannel.class, consumer, representationKeepAlive2));
-    } else {
-      actor.deadLetters().failedDelivery(new DeadLetter(actor, representationKeepAlive2));
-    }
-  }
-
-  @Override
   public void respondWith(RequestResponseContext<?> context, final ConsumerByteBuffer buffer) {
     respondWith(context, buffer, false);
   }
@@ -56,9 +45,9 @@ public class ResponseSenderChannel__Proxy implements ResponseSenderChannel {
   public void respondWith(RequestResponseContext<?> context, final ConsumerByteBuffer buffer, final boolean closeFollowing) {
     if (!actor.isStopped()) {
       final SerializableConsumer<ResponseSenderChannel> consumer = (actor) -> actor.respondWith(context, buffer);
-      mailbox.send(new LocalMessage<ResponseSenderChannel>(actor, ResponseSenderChannel.class, consumer, representationRespondWith3));
+      mailbox.send(new LocalMessage<ResponseSenderChannel>(actor, ResponseSenderChannel.class, consumer, representationRespondWith2));
     } else {
-      actor.deadLetters().failedDelivery(new DeadLetter(actor, representationRespondWith3));
+      actor.deadLetters().failedDelivery(new DeadLetter(actor, representationRespondWith2));
     }
   }
 }
