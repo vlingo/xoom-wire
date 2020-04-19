986edbb1ddb923977151e1662822543df53658cd
VaughnVernon <vaughn@forcomprehension.com>
2020-04-19 01:05:41 -0700

    Removed explicitClose and debug logging.

--- src/main/java/io/vlingo/wire/fdx/bidirectional/netty/server/NettyServerChannelContext.java
+++ src/main/java/io/vlingo/wire/fdx/bidirectional/netty/server/NettyServerChannelContext.java
@@ -6,7 +6,7 @@
 // one at https://mozilla.org/MPL/2.0/.
 package io.vlingo.wire.fdx.bidirectional.netty.server;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.vlingo.wire.channel.RequestResponseContext;
@@ -14,7 +14,7 @@ import io.vlingo.wire.channel.ResponseSenderChannel;
 import io.vlingo.wire.message.ConsumerByteBuffer;
 
 final class NettyServerChannelContext implements RequestResponseContext<ConsumerByteBuffer> {
-  private static AtomicInteger contextId = new AtomicInteger(0);
+  private static AtomicLong contextId = new AtomicLong(0);
 
   private final ChannelHandlerContext nettyChannelContext;
   @SuppressWarnings("unused")
@@ -23,36 +23,20 @@ final class NettyServerChannelContext implements RequestResponseContext<Consumer
   private final String id;
   private ResponseSenderChannel sender;
 
-//  private boolean explicitClose;
-
   NettyServerChannelContext(final ChannelHandlerContext nettyChannelContext, final ResponseSenderChannel sender) {
     this.nettyChannelContext = nettyChannelContext;
     this.sender = sender;
-
-//    this.explicitClose = true;
     this.id = "" + contextId.incrementAndGet();
-  }
-
-  void eagerClose() {
-//     if (explicitClose) return;
-//     System.out.println(">>>>>>>>>>>>>>>>>>>>> NettyServerChannelContext::eagerClose()");
-//     final ChannelHandlerContext channelHandlerContext = getNettyChannelContext();
-//     if (channelHandlerContext.channel().isOpen()) {
-//       channelHandlerContext.flush();
-//       channelHandlerContext.close();
-//     }
   }
 
   @Override
   @SuppressWarnings("unchecked")
   public <T> T consumerData() {
-    System.out.println("########### CHANNEL CONTEXT GET CONSUMER DATA (" + id + "): " + consumerData);
     return (T) consumerData;
   }
 
   @Override
   public <T> T consumerData(final T workingData) {
-    System.out.println("########### CHANNEL CONTEXT SET CONSUMER DATA (" + id + "): " + workingData);
     this.consumerData = workingData;
     return workingData;
   }
@@ -67,10 +51,6 @@ final class NettyServerChannelContext implements RequestResponseContext<Consumer
     return id;
   }
 
-  void requireExplicitClose(final boolean option) {
-//    explicitClose = option;
-  }
-
   @Override
   public ResponseSenderChannel sender() {
     return this.sender;
@@ -82,10 +62,6 @@ final class NettyServerChannelContext implements RequestResponseContext<Consumer
   }
 
   ChannelHandlerContext getNettyChannelContext() {
-    if (!nettyChannelContext.channel().isOpen()) {
-      System.out.println(">>>>>>>>>>>>>>>>>>>>> AFTER CLOSE: NettyServerChannelContext::getNettyChannelContext()");
-      throw new IllegalStateException("NettyServerChannelContext: Channel closed");
-    }
     return nettyChannelContext;
   }
 }
