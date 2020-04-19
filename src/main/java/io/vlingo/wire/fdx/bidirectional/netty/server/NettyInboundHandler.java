ea7c5116a43f47ebd39389d04767ca35da61cbb6
VaughnVernon <vaughn@forcomprehension.com>
2020-04-19 01:02:36 -0700

    Ensure close after write-flush. Remove unregistered context. Logging.

--- src/main/java/io/vlingo/wire/fdx/bidirectional/netty/server/NettyInboundHandler.java
+++ src/main/java/io/vlingo/wire/fdx/bidirectional/netty/server/NettyInboundHandler.java
@@ -8,7 +8,7 @@ package io.vlingo.wire.fdx.bidirectional.netty.server;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +17,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.EmptyByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
@@ -30,23 +31,24 @@ import io.vlingo.wire.message.ConsumerByteBufferPool;
 
 final class NettyInboundHandler extends ChannelInboundHandlerAdapter implements ResponseSenderChannel {
   private final static Logger logger = LoggerFactory.getLogger(NettyInboundHandler.class);
+
   private final RequestChannelConsumer consumer;
+  private String contextInstanceId;
+  private final Map<String, NettyServerChannelContext> contexts;
   private final ConsumerByteBufferPool readBufferPool;
 
-  private static final AtomicInteger nextInstanceId = new AtomicInteger();
-  private final int instanceId;
+  private static final AtomicLong nextInstanceId = new AtomicLong(0);
+  private final long instanceId;
 
   NettyInboundHandler(final RequestChannelConsumerProvider consumerProvider, final int maxBufferPoolSize, final int maxMessageSize) {
     this.consumer = consumerProvider.requestChannelConsumer();
     this.readBufferPool = new ConsumerByteBufferPool(ElasticResourcePool.Config.of(maxBufferPoolSize), maxMessageSize);
-
+    this.contexts = new HashMap<>();
     this.instanceId = nextInstanceId.incrementAndGet();
-    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>> NettyInboundHandler::ctor(): " + instanceId + " CONSUMER: " + consumer);
   }
 
-  private final Map<String, NettyServerChannelContext> contexts = new HashMap<>();
   @Override
-  public void channelRead(final ChannelHandlerContext ctx, final Object msg) {
+  public void channelRead(final ChannelHandlerContext context, final Object msg) {
     if (msg == null || msg == Unpooled.EMPTY_BUFFER || msg instanceof EmptyByteBuf) {
       return;
     }
@@ -54,16 +56,18 @@ final class NettyInboundHandler extends ChannelInboundHandlerAdapter implements 
       logger.debug("Request received");
     }
 
-    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>> NettyInboundHandler::channelRead(): " + instanceId);
+    logger.debug(">>>>> NettyInboundHandler::channelRead(): " + instanceId + " NAME: " + contextInstanceId(context));
 
     try {
-      NettyServerChannelContext channelContext = contexts.get(ctx.name());
+      final String contextInstanceId = contextInstanceId(context);
+      NettyServerChannelContext channelContext = contexts.get(contextInstanceId);
       if (channelContext == null) {
-        channelContext = new NettyServerChannelContext(ctx, this);
-        contexts.put(ctx.name(), channelContext);
+        channelContext = new NettyServerChannelContext(context, this);
+        contexts.put(contextInstanceId, channelContext);
       }
 
       final ConsumerByteBuffer pooledBuffer = readBufferPool.acquire("NettyClientHandler#channelRead");
+
       try {
         final ByteBuf byteBuf = (ByteBuf) msg;
         byte[] bytes = new byte[byteBuf.readableBytes()];
@@ -78,28 +82,30 @@ final class NettyInboundHandler extends ChannelInboundHandlerAdapter implements 
       }
     } catch (Throwable throwable) {
       logger.error("Error reading the incoming data.", throwable);
-      ctx.close();
+      context.close();
     } finally {
       ReferenceCountUtil.release(msg);
     }
   }
 
   @Override
-  public void exceptionCaught(final ChannelHandlerContext ctx, final Throwable cause) throws Exception {
-    super.exceptionCaught(ctx, cause);
+  public void channelUnregistered(final ChannelHandlerContext context) throws Exception {
+    logger.debug(">>>>> NettyInboundHandler::channelUnregistered(): " + instanceId + " NAME: " + contextInstanceId(context));
+    contexts.remove(contextInstanceId(context));
+    super.channelUnregistered(context);
+  }
+
+  @Override
+  public void exceptionCaught(final ChannelHandlerContext context, final Throwable cause) throws Exception {
     logger.error("Unexpected exception", cause);
+    super.exceptionCaught(context, cause);
   }
 
   @Override
   public void abandon(final RequestResponseContext<?> context) {
-    logger.trace(">>>>>>>>>>>>>>>>>>>>>>>>>>>> NettyInboundHandler::abandon(): " + instanceId);
     final ChannelHandlerContext nettyChannelContext = ((NettyServerChannelContext) context).getNettyChannelContext();
+    logger.debug(">>>>> NettyInboundHandler::abandon(): " + instanceId + " NAME: " + contextInstanceId(nettyChannelContext));
     nettyChannelContext.close();
-  }
-
-  @Override
-  public void explicitClose(final RequestResponseContext<?> context, final boolean option) {
-
   }
 
   @Override
@@ -109,34 +115,40 @@ final class NettyInboundHandler extends ChannelInboundHandlerAdapter implements 
 
   @Override
   public void respondWith(final RequestResponseContext<?> context, final ConsumerByteBuffer buffer, final boolean closeFollowing) {
-    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>> NettyInboundHandler::respondWith(): " + instanceId + " : CLOSE? " + closeFollowing);
-
     final NettyServerChannelContext nettyServerChannelContext = (NettyServerChannelContext) context;
-    final ChannelHandlerContext nettyChannelContext = nettyServerChannelContext.getNettyChannelContext();
+    final ChannelHandlerContext channelHandlerContext = nettyServerChannelContext.getNettyChannelContext();
 
-    final ByteBuf replyBuffer = nettyChannelContext.alloc()
-                                                   .buffer(buffer.limit());
+    logger.debug(">>>>> NettyInboundHandler::respondWith(): " + instanceId + " NAME: " + contextInstanceId(channelHandlerContext) + " : CLOSE? " + closeFollowing);
+
+    final ByteBuf replyBuffer = channelHandlerContext.alloc().buffer(buffer.limit());
+
     replyBuffer.writeBytes(buffer.asByteBuffer());
 
-    try {
-      final ChannelFuture writeCompletable = nettyChannelContext.writeAndFlush(replyBuffer);
+    channelHandlerContext
+      .writeAndFlush(replyBuffer)
+      .addListener(new ChannelFutureListener() {
+        @Override
+        public void operationComplete(final ChannelFuture future) throws Exception {
+          if (future.isSuccess()) {
+            logger.debug("Reply sent");
+          } else {
+            logger.error("Failed to send reply", future.cause());
+          }
+          if (closeFollowing) {
+            channelHandlerContext
+              .close()
+              .addListener(closeFuture -> {
+                logger.debug(">>>>> NettyInboundHandler::respondWith(): " + instanceId + " NAME: " + contextInstanceId(channelHandlerContext) + " : CLOSED");
+            });
+          }
+        }
+      });
+  }
 
-      writeCompletable
-        .addListener(future -> {
-                           if (future.isSuccess()) {
-                             logger.trace("Reply sent");
-                           } else {
-                             logger.error("Failed to send reply", future.cause());
-                           }
-                         });
-
-      writeCompletable.await().syncUninterruptibly();
-      if (writeCompletable.isDone() && closeFollowing) {
-        nettyChannelContext.close();
-      }
-
-    } catch (InterruptedException e) {
-      logger.trace("Timeout: NettyInboundHandler::respondWith() writeAndFlush().await()");
+  private String contextInstanceId(final ChannelHandlerContext context) {
+    if (contextInstanceId == null) {
+      contextInstanceId = context.name() + ":" + instanceId;
     }
+    return contextInstanceId;
   }
 }
