1a004ee3b39b29abc4ac1fabb8b5368a497461a3
Hamza JGUERIM <hamzajg@gmail.com>
2021-05-11 04:25:16 +0100

    Fixing Indentation..

--- src/main/java/io/vlingo/xoom/wire/implnative/NativeImpl.java
+++ src/main/java/io/vlingo/xoom/wire/implnative/NativeImpl.java
@@ -8,13 +8,13 @@ import org.graalvm.nativeimage.c.type.CCharPointer;
 import org.graalvm.nativeimage.c.type.CTypeConversion;
 
 public final class NativeImpl {
-    @CEntryPoint(name = "Java_io_vlingo_xoom_wirenative_Native_with")
-    public static int with(@CEntryPoint.IsolateThreadContext long isolateId, CCharPointer name) {
-        final String nameString = CTypeConversion.toJavaString(name);
-        RefreshableSelector.withNoThreshold(Logger.basicLogger());
-        Node.with(Node.NO_NODE.id(), Node.NO_NODE.name(), Node.NO_NODE.applicationAddress().host(),
-                Node.NO_NODE.applicationAddress().NO_PORT,
-                Node.NO_NODE.applicationAddress().NO_PORT);
-        return 0;
-    }
+  @CEntryPoint(name = "Java_io_vlingo_xoom_wirenative_Native_with")
+  public static int with(@CEntryPoint.IsolateThreadContext long isolateId, CCharPointer name) {
+    final String nameString = CTypeConversion.toJavaString(name);
+    RefreshableSelector.withNoThreshold(Logger.basicLogger());
+    Node.with(Node.NO_NODE.id(), Node.NO_NODE.name(), Node.NO_NODE.applicationAddress().host(),
+        Node.NO_NODE.applicationAddress().NO_PORT,
+        Node.NO_NODE.applicationAddress().NO_PORT);
+    return 0;
+  }
 }
